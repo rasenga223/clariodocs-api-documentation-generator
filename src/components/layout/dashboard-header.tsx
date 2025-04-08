c26e3c17c0cc4ac89d01d2d8ee2b06d4819c84ec
AJ1732 <ejemeniboi@gmail.com>
2025-04-09 00:03:30 +0100

    chore: add buttons to the dashboard header

--- src/components/layout/dashboard-header.tsx
+++ src/components/layout/dashboard-header.tsx
@@ -1,10 +1,16 @@
 import { DecorativeSquare } from "@/components/elements/decorative-square";
+import { Button } from "@/components/ui/button";
 
 export const DashboardHeader = () => {
   return (
-    <header className="relative border-b px-4 py-4">
+    <header className="relative flex items-center justify-between border-b px-4 py-4">
       <DecorativeSquare className="-bottom-px -left-px w-2 border-b border-l" />
       <h2> Dashboard Header</h2>
+
+      <div className="flex gap-2">
+        <Button variant={"ghost"}>Log in</Button>
+        <Button>Sign Up</Button>
+      </div>
     </header>
   );
 };
