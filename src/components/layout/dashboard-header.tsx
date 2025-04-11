fa7c39342a603567134f547ff0e35fa493624458
AJ1732 <ejemeniboi@gmail.com>
2025-04-11 23:02:15 +0100

    style: update the text color

--- src/components/layout/dashboard-header.tsx
+++ src/components/layout/dashboard-header.tsx
@@ -17,7 +17,7 @@ export const DashboardHeader = () => {
 
   return (
     <header className="flex items-center justify-between border-b px-4 py-3">
-      <h1 className="font-bold">{title}</h1>
+      <h1 className="font-bold text-zinc-300">{title}</h1>
       <DashboardHeaderAuth />
       <SidebarMobileToggle />
     </header>
