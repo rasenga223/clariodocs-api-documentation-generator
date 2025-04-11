ca656a41f574a809020cc90583608941279fae26
AJ1732 <ejemeniboi@gmail.com>
2025-04-11 14:25:21 +0100

    chore: update the loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-import '@/app/globals.css';
+import "@/app/globals.css";
 import type React from "react";
-import { Suspense } from 'react';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Suspense } from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
+import { Loader } from "@/components/elements/loader";
 import { Providers } from "@/provider";
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-geist-sans' });
+const inter = Inter({ subsets: ["latin"], variable: "--font-geist-sans" });
 
 export const metadata: Metadata = {
-  title: 'API Documentation Generator',
-  description: 'Create beautiful, interactive API documentation with ease',
+  title: "API Documentation Generator",
+  description: "Create beautiful, interactive API documentation with ease",
 };
 
 function Loading() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-10 h-10 border-4 rounded-full border-primary animate-spin border-t-transparent"></div>
+    <div className="flex min-h-screen items-center justify-center">
+      <Loader />
     </div>
   );
 }
@@ -28,11 +29,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className}  dark antialiased bg-background text-foreground`}>
+      <body
+        className={`${inter.className} dark bg-background text-foreground antialiased`}
+      >
         <Suspense fallback={<Loading />}>
-          <Providers>
-            {children}
-          </Providers>
+          <Providers>{children}</Providers>
         </Suspense>
         <Toaster />
       </body>
